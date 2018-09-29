bf433467b9153002f38c90657a12084c1aa67774
Andras Wirth <agrippa.wia@gmail.com>
2018-09-29 11:30:42 +0200

    initial state

---
+++ src/main/java/hu/wirthandras/firefly/FireFlyApplication.java
@@ -0,0 +1,12 @@
+package hu.wirthandras.firefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireFlyApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/wirthandras/firefly/FireFlyApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.wirthandras.firefly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FireFlyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
